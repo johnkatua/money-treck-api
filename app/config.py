17ed005f9401330425be1ca17ba94fff19703ff3
johnkatua <johnkatua99@gmail.com>
2024-12-16 13:45:55 +0300

    update settings class

--- app/config.py
+++ app/config.py
@@ -2,7 +2,13 @@ from pydantic import BaseModel, EmailStr
 
 
 class Settings(BaseModel):
-    pass
+    DATABASE_URL: str
+
+    REFRESH_TOKEN_EXPIRES_IN: int
+    ACCESS_TOKEN_EXPIRES_IN: int
+    JWT_ALGORITHM: str
+    class Config:
+        env_file = './.env'
 
 
 settings = Settings()
