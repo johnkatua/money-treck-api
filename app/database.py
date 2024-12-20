729756e82ccef60b77370001a0b8707894280f76
johnkatua <johnkatua99@gmail.com>
2024-12-20 15:06:23 +0300

    add users table and create index for the users table

--- app/database.py
+++ app/database.py
@@ -1,5 +1,11 @@
 from pymongo import mongo_client
+import pymongo
 from app.config import settings
 
 client = mongo_client.MongoClient(settings.DATABASE_URL)
 print('Connect to MongoDB...')
+
+db = client[settings.MONGO_DB]
+User = db.users
+
+User.create_index([("email", pymongo.ASCENDING)], unique=True)
